41a8749723efcba864ede99482b57ad83571d188
Thierrylawrence <thierry.lawrence24@trinityschoolnyc.org>
2023-05-14 06:55:10 -0400

    everything except the lines done

--- server.py
+++ server.py
@@ -13,14 +13,23 @@ def index():
     data = json.load(f)
     f.close()
 
-    selected_option = 2001
-    return render_template('index.html', data = data, selected_option=selected_option)
+    requested_year = request.args.get('year1')
+
+
+
+
+
+
+    return render_template('index.html', data = data, year = requested_year)
 
 @app.route('/year')
 def year():
 
+    f = open("templates/life_expectancy.json", "r")
+    data = json.load(f)
+    f.close()
 
     requested_year = request.args.get('year')
-    return render_template('year.html', year = requested_year)
+    return render_template('year.html', year = requested_year, data = data)
 
 app.run(debug=True)
